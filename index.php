87b281266a3cdccfb10b18e0aab614d3de2414da
brianjrosenthal <brian.rosenthal@gmail.com>
2025-10-04 18:02:16 -0400

    fix homepage

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ header_html('Home');
 <div class="card">
   <h2>Welcome to <?= h($siteTitle) ?></h2>
   <p>Hello, <?= h($me['first_name'] ?? '') ?>!</p>
-  <p>This is your RAG (Retrieval Augmented Generation) knowledge base application. Here you'll be able to upload documents and build a custom GPT that responds with answers from those documents.</p>
-  <p class="small">The document upload and RAG functionality will be implemented in future phases.</p>
+  <p>Welecome to your new web portal.</p>
+  <p class="small">Change this in the sourcecode as you build out your homepage.</p>
 </div>
 
 <?php footer_html(); ?>
